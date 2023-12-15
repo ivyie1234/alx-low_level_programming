4475f2e898dd995b47778056090cdfb608c1f957
ivyie1234 <osmerahyvonne@gmail.com>
2023-12-15 01:23:12 -0800

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+
 /**
  * main - Entry point
  *
@@ -11,7 +12,7 @@ int main(void)
 	long int c;
 	long long int d;
 	float f;
-	
+
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
