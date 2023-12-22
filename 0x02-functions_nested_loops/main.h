7cfdc9aad8c6b3ce6dd9268dbb813e294343a3b5
ivyie1234 <osmerahyvonne@gmail.com>
2023-12-21 23:43:14 -0800

    task0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,8 @@
-#include<stdio.h>
-#define MAIN_H
 #ifndef MAIN_H
-#include<unistd>
+#define MAIN_H
+#include <stdio.h>
 
-int main(void);
-int _putchar(char);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
