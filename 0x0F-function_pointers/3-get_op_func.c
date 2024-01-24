2885512e05187bd3780c0e30051c8116c243dfec
ivyie1234 <osmerahyvonne@gmail.com>
2024-01-24 12:26:19 -0800

    task3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -7,7 +7,7 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-	op_t ops[] = {
+	op_t op_s[] = {
 		{"+", op_add},
 		{"-", op_sub},
 		{"*", op_mul},
@@ -18,10 +18,10 @@ int (*get_op_func(char *s))(int, int)
 
 	int i = 0;
 
-	while (ops[i].op != NULL)
+	while (ops[i].op)
 	{
-		if (*(ops[i].op) == *s && *(s + 1) == '\0')
-			return (ops[i].f);
+		if (*(op_s[i].op) == *s)
+			return (op_s[i].f);
 		i++;
 	}
 	return (NULL);
