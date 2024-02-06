b07d7c3090b65b4a7729873fd4551c8ac107645b
ivyie1234 <osmerahyvonne@gmail.com>
2024-02-06 11:14:28 -0800

    task8

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -11,15 +11,14 @@ int pop_listint(listint_t **head)
 	int data = 0;
 	listint_t *temp;
 
-	if (head == NULL || *head == NULL) {
-		return 0;
-	}
+	if (head == NULL || *head == NULL)
+		return (0);
 
 	temp = *head;
 	data = temp->n;
 	*head = temp->next;
 	free(temp);
 
-	return data;
+	return (data);
 }
 
--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -19,6 +19,6 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (i <= index)
 		return (NULL);
 
-	return current;
+	return (current);
 }
 
